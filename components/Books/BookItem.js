08fedb14a7ab686ce3eaecdba0a5d85b8a94a9aa
mjarafalconi <mjarafalconi@gmail.com>
2023-03-15 21:04:21 -0500

    Primer commit 15032023

---
+++ components/Books/BookItem.js
@@ -0,0 +1,215 @@
+//MAIN DEPENDENCIES
+import {
+  Button,
+  IconButton,
+  Grid,
+  Stack,
+  Typography,
+  Box,
+} from "@mui/material";
+import AddToCartIcon from "@mui/icons-material/AddShoppingCart";
+import QtyManager from "../Cart/QtyManager";
+
+//ADD DEPENDENCIES
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import CloseIcon from '@mui/icons-material/Close';
+
+//This function is a styled-component that customizes the padding 
+//of the content and actions of a Material UI dialog based on the 
+//provided theme.
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+//This code defines a custom BootstrapDialogTitle component that 
+//renders a DialogTitle with additional close button functionality. 
+//It destructures the onClose and children props, and applies custom 
+//styles using sx property from Material-UI.
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+//Main components
+const BookItem = ({
+  name,
+  genre,
+  image,
+  price,
+  description,
+  alreadyInCart,
+  qty,
+  increaseQty,
+  decreaseQty,
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Grid item xs={12} lg={4}>
+      <Stack
+        sx={{
+          ":hover .book-cover": {
+            top: -40,
+          },
+        }}
+        direction="row"
+        spacing={3}
+        alignItems="flex-start"
+      >
+        {/* left stack */}
+        <Stack position="relative" width={130}>
+          <Stack
+            className="book-cover"
+            top={-25}
+            position="relative"
+            left={22.5}
+            sx={(theme) => ({
+              background: `url(${image})`,
+              ...theme.mixins.bookCover,
+            })}
+          />
+          <Stack
+            zIndex={-1}
+            position="absolute"
+            bgcolor="#F3EEF0"
+            width={130}
+            height={120}
+            borderRadius={2}
+          />
+
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Más detalle
+          </Button>
+          <BootstrapDialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
+          >
+            <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+              {name}
+            </BootstrapDialogTitle>
+
+            <DialogContent dividers>
+              <Grid container spacing={4}
+                wrap="nowrap"
+                direction="row"
+                justifyContent="center"
+                alignItems="center">
+                <Grid item xs={6}>
+                  <Stack
+                    sx={(theme) => ({
+                      background: `url(${image})`,
+                      ...theme.mixins.bookCover,
+                      width: 56 * 5,
+                      height: 83 * 5,
+
+                    })}
+                  />
+                  <Stack
+                  />
+                </Grid>
+                <Grid item xs={6}>
+
+                  <Typography wrap variant="body2" align="justify">
+                    {description}
+                  </Typography>
+                </Grid>
+
+              </Grid>
+            </DialogContent>
+
+          </BootstrapDialog>
+
+        </Stack>
+        {/* right stack */}
+        <Stack position="relative" top={-25} spacing={1}>
+          <Box
+            maxWidth={{
+              xs: 200,
+              lg: 240,
+            }}
+          >
+            <Typography noWrap variant="h5">
+              {name}
+            </Typography>
+          </Box>
+          <Typography variant="body2" className="lato" fontStyle="bold">
+            Tipo: {genre}
+          </Typography>
+          {/*<Typography variant="body2" className="lato" fontStyle="bold">
+            Piel: {skinType}
+          </Typography>
+          <Stack color="#C99100" direction="row">
+            <StarIcon /> <Typography className="lato">{stars}</Typography>
+          </Stack>*/}
+          <Stack height={38} direction="row" alignItems="center">
+            <Typography mr={3} className="lato">
+              S/ {price}
+            </Typography>
+            {alreadyInCart ? (
+              <QtyManager
+                qty={qty}
+                decreaseQty={decreaseQty}
+                increaseQty={increaseQty}
+              />
+            ) : (
+              <IconButton
+                onClick={increaseQty}
+                sx={{
+                  bgcolor: "black !important",
+                  color: "#fff !important",
+                  border: "1px solid rgba(0, 0, 0, 0.08)",
+                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.08)",
+                }}
+              >
+                <AddToCartIcon fontSize="small" />
+              </IconButton>
+            )}
+          </Stack>
+        </Stack>
+      </Stack>
+    </Grid>
+  );
+};
+
+export default BookItem;
---
+++ pages/navbar.js
@@ -0,0 +1,239 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {
+    Stack,
+    Tabs,
+    Tab,
+    tabClasses,
+    tabsClasses,
+} from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+/* export default function ButtonAppBar({ genres, activeGenre, changeGenre, openPanel }) {
+    return (
+        <Box
+            mb={{
+                xs: 10,
+                lg: 15,
+            }}
+            pt={{
+                xs: 2,
+                lg: 6,
+            }}
+            px={{
+                xs: 2,
+                lg: 6,
+            }}
+        >
+            <AppBar >
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        FutuLabs
+                    </Typography>
+                    <Tabs
+                        variant="scrollable"
+                        visibleScrollbar
+                        onChange={(_, value) => changeGenre(value)}
+                        value={activeGenre}
+                        sx={{
+                            [`& .${tabClasses.root}`]: {
+                                fontWeight: 600,
+                                fontFamily: "Roboto",
+                                textTransform: "capitalize",
+                                fontSize: 17,
+                            },
+                            [`& .${tabsClasses.indicator}`]: {
+                                height: 4,
+                            },
+                        }}
+                    >
+                        <Tab value="All" label="All" />
+                        {genres.map((genre) => (
+                            <Tab key={genre} value={genre} label={genre} />
+                        ))}
+                    </Tabs>
+                    <Button color="inherit">Login</Button>
+                    <IconButton onClick={openPanel}>
+                        <ShoppingCartIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+} */
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from "react-router-dom";
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        FutuLabs
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">
+                                        <Link style={{ textDecoration: "none", color: "white" }} to={`/${page}`}>
+                                            {page}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        FutuLabs
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                <Link style={{ textDecoration: "none", color: "white" }} to={`/${page}`}>
+                                    {page}
+                                </Link>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
+    );
+}
+export default ResponsiveAppBar;
