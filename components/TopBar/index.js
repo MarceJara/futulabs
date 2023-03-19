513d945f4ca06b25e3e585a2f614bb988f13150b
mjarafalconi <mjarafalconi@gmail.com>
2023-03-19 11:41:42 -0500

    Se arreglo el carrito en la pagina de productos

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -1,6 +1,5 @@
 import {
   Stack,
-  Typography,
   IconButton,
   Tabs,
   Tab,
@@ -12,6 +11,44 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 const TopBar = ({ genres, activeGenre, changeGenre, openPanel }) => {
   return (
     <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center">
+      <Tabs
+        variant="scrollable"
+        visibleScrollbar
+        onChange={(_, value) => changeGenre(value)}
+        value={activeGenre}
+        sx={{
+          [`& .${tabClasses.root}`]: {
+            fontWeight: 600,
+            fontFamily: "Roboto",
+            textTransform: "capitalize",
+            fontSize: 17,
+          },
+          [`& .${tabsClasses.indicator}`]: {
+            height: 4,
+          },
+        }}
+      >
+        <Tab value="All" label="All" />
+        {genres.map((genre) => (
+          <Tab key={genre} value={genre} label={genre} />
+        ))}
+      </Tabs>
+      <IconButton onClick={openPanel}>
+        <ShoppingCartIcon />
+      </IconButton>
+    </Stack >
+
+
+  );
+};
+
+export default TopBar;
+
+/* {/* <Stack
+      direction="row"
       mb={{
         xs: 10,
         lg: 15,
@@ -24,19 +61,9 @@ const TopBar = ({ genres, activeGenre, changeGenre, openPanel }) => {
         xs: 2,
         lg: 6,
       }}
-      bgcolor="#DCF5FF"
+      bgcolor="inherit"
     >
-      <Stack
-        mb={6}
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Typography variant="h3">FutuLabs</Typography>
-        <IconButton onClick={openPanel}>
-          <ShoppingCartIcon />
-        </IconButton>
-      </Stack>
+
 
       <Tabs
         variant="scrollable"
@@ -60,8 +87,16 @@ const TopBar = ({ genres, activeGenre, changeGenre, openPanel }) => {
           <Tab key={genre} value={genre} label={genre} />
         ))}
       </Tabs>
-    </Stack>
-  );
-};
+      <Stack
+        mb={1}
+        direction="row"
+        alignItems="right"
+        justifyContent="space-between"
+      >
+        
+  <IconButton onClick={openPanel}>
+    <ShoppingCartIcon />
+  </IconButton>
+      </Stack >
 
-export default TopBar;
+    </Stack > * /} */
